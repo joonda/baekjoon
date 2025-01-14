35de4d22424fc8e95b4e04ad9d8764d1e7942c18
HJ Lee <143615224+joonda@users.noreply.github.com>
2025-01-14 09:13:53 +0900

    [Bronze V] Title: N 찍기, Time: 844 ms, Memory: 29764 KB -BaekjoonHub

---
+++ 백준/Bronze/2741. N 찍기/N 찍기.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+        st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        for (int i=0; i<N; i++) {
+            bw.write(i+1 + "\n");
+            bw.flush();
+        }
+    }
+}
