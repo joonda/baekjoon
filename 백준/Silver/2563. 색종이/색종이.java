48ab41de373f5ba41b4f961af9de234eca6ba4d4
HJ Lee <143615224+joonda@users.noreply.github.com>
2025-01-18 01:32:59 +0900

    [Silver V] Title: 색종이, Time: 116 ms, Memory: 15840 KB -BaekjoonHub

---
+++ 백준/Silver/2563. 색종이/색종이.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+
+        int[][] arr = new int[100][100];
+
+        st = new StringTokenizer(br.readLine());
+        int T = Integer.parseInt(st.nextToken());
+        int x, y;
+
+        for (int i=1; i<=T; i++) {
+            st =  new StringTokenizer(br.readLine());
+            x = Integer.parseInt(st.nextToken());
+            y = Integer.parseInt(st.nextToken());
+
+            for (int j=x; j<x+10; j++) {
+                for (int k=y; k<y+10; k++) {
+                    arr[j][k] = 1;
+                }
+            }
+        }
+        int width =0;
+        for (int i=0; i<100; i++) {
+            for (int j=0; j<100; j++) {
+                if (arr[i][j] == 1) {
+                    width += 1;
+                }
+            }
+        }
+        bw.write(width+"\n");
+        bw.flush();
+    }
+}
