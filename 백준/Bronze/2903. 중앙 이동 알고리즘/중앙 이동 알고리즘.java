55dda1ef63dd603c4d0892c49f0ec1dcbbcd5110
HJ Lee <143615224+joonda@users.noreply.github.com>
2025-01-22 10:27:50 +0900

    [Bronze III] Title: 중앙 이동 알고리즘, Time: 104 ms, Memory: 14092 KB -BaekjoonHub

---
+++ 백준/Bronze/2903. 중앙 이동 알고리즘/중앙 이동 알고리즘.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        br.close();
+
+        System.out.println((int)Math.pow(Math.pow(2, N)+1, 2));
+    }
+}
