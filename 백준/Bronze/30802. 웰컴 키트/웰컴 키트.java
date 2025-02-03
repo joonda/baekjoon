c544c3e4565203c89f85c11c6b20e82cff29d303
HJ Lee <143615224+joonda@users.noreply.github.com>
2025-02-04 00:39:27 +0900

    [Bronze III] Title: 웰컴 키트, Time: 124 ms, Memory: 15888 KB -BaekjoonHub

---
+++ 백준/Bronze/30802. 웰컴 키트/웰컴 키트.java
@@ -0,0 +1,51 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+
+        int[] clothCount = new int[]{Integer.parseInt(st.nextToken()),
+                Integer.parseInt(st.nextToken()),
+                Integer.parseInt(st.nextToken()),
+                Integer.parseInt(st.nextToken()),
+                Integer.parseInt(st.nextToken()),
+                Integer.parseInt(st.nextToken())};
+
+        st = new StringTokenizer(br.readLine());
+        int T = Integer.parseInt(st.nextToken());
+        int P = Integer.parseInt(st.nextToken());
+
+        int totalBundles = 0;
+        for (int i=0; i<clothCount.length; i++) {
+            totalBundles += convertShirtBundles(clothCount[i], T);
+        }
+
+        int[] penResult = distributePens(N, P);
+
+        System.out.println(totalBundles);
+        System.out.println(penResult[0] + " " + penResult[1]);
+    }
+
+    public static int convertShirtBundles(int shirtCount, int shirtBundle) {
+        int shirtbundle = shirtCount / shirtBundle;
+        if ((shirtCount % shirtBundle) != 0) {
+            shirtbundle++;
+        }
+
+        return shirtbundle;
+    }
+
+    public static int[] distributePens(int participants, int pens) {
+        int bundles = participants / pens;
+        int remainingPens = participants % pens;
+
+        return new int[]{bundles, remainingPens};
+    }
+}
