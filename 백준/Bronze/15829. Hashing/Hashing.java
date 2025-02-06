467445982393b8bfa7ad82c5fe9d6e12339a3e31
HJ Lee <143615224+joonda@users.noreply.github.com>
2025-02-06 17:27:27 +0900

    [Bronze II] Title: Hashing, Time: 104 ms, Memory: 14212 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.java
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -8,14 +8,18 @@ public class Main {
 
         st = new StringTokenizer(br.readLine());
         int L = Integer.parseInt(st.nextToken());
-        int r = 31;
 
         st = new StringTokenizer(br.readLine());
         String str = st.nextToken();
-        int sum = 0;
+        long sum = 0;
+        long pow = 1;
         for (int i=0; i<L; i++) {
-            int ch = str.charAt(i)-'`';
-            sum += (ch)*(Math.pow(r, i));
+            int ch = str.charAt(i)- 96;
+            // sum에는 문자열을 숫자로 변환한 값에 pow를 곱하고, 이를 M으로 나눈 나머지를 더한다
+            // overflow 방지위해 M을 나눈 나머지를 표현.
+            sum = (sum + ch * pow) % 1234567891;
+            // pow는 r을 지속적으로 곱한 값에 M을 나눈 나머지 표현
+            pow = (pow * 31) % 1234567891;
         }
         System.out.println(sum);
     }
