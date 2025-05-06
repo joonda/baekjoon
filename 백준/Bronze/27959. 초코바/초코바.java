67a11ca1cbc85b5d040dfcd211558dba89498340
HJ Lee <143615224+joonda@users.noreply.github.com>
2025-05-06 21:13:08 +0900

    [Bronze V] Title: 초코바, Time: 104 ms, Memory: 14132 KB -BaekjoonHub

---
+++ 백준/Bronze/27959. 초코바/초코바.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        if (100 * n < m) {
+            System.out.println("No");
+        } else {
+            System.out.println("Yes");
+        }
+    }
+}
