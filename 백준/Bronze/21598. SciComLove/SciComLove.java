bfbdbb77b14db684bffc4fae3eae8cf93697f48d
HJ Lee <143615224+joonda@users.noreply.github.com>
2025-06-16 22:46:10 +0900

    [Bronze V] Title: SciComLove, Time: 100 ms, Memory: 13988 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/21598. SciComLove/SciComLove.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new java.io.InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        
+        if (N == 1) {
+            System.out.println("SciComLove");
+        } else if (N == 2) {
+            System.out.println("SciComLove");
+            System.out.println("SciComLove");
+        } 
+    }
+}
